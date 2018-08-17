ecb7f4b137695066ba447097d1ab28b5639e8580
Nandan Rao <nandanmarkrao@gmail.com>
2018-08-17 21:47:52 +0200

    new service start scripts

--- hyper-service-start-t.sh
+++ hyper-service-start-t.sh
@@ -1,3 +1,3 @@
-hyper service create --env-file "./.test-env" --size S2 --protocol http --service-port 80 --container-port 5000 --name healthworkers-test --label=app=healthworkers-test --replicas 1 nandanrao/healthworkers
+hyper service create --env-file "./.env-test" --size S2 --protocol http --service-port 80 --container-port 5000 --name healthworkers-test --label=app=healthworkers-test --replicas 1 nandanrao/hw-server
 sleep 10
 hyper service attach-fip --fip=209.177.88.233 healthworkers-test
--- hyper-service-start.sh
+++ hyper-service-start.sh
@@ -1,3 +1,3 @@
-hyper service create --env-file ./.env --size S2 --service-port 80 --container-port 5000 --name healthworkers --label=app=healthworkers --replicas 2 nandanrao/healthworkers
+hyper service create --env-file "./.env" --size S2 --protocol http --service-port 80 --container-port 5000 --name healthworkers --label=app=healthworkers --replicas 2 nandanrao/hw-server
 sleep 10
 hyper service attach-fip --fip=209.177.93.17 healthworkers
