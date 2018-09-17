6f5c188d46f3d5c4fd5d2a34ad5df339e7207b98
Nandan <nandanmarkrao@gmail.com>
2018-09-17 16:53:59 +0200

    service with ssl

--- hyper-service-start.sh
+++ hyper-service-start.sh
@@ -1,3 +1,3 @@
-hyper service create --env-file ./.env --size S2 --service-port 80 --container-port 5000 --name healthworkers --label=app=healthworkers --replicas 2 nandanrao/healthworkers
+hyper service create --env-file ./.env --size S2 --service-port 443 --container-port 5000 --name healthworkers --ssl-cert=ssl.pem --protocol httpsTerm --label=app=healthworkers --replicas 2 nandanrao/healthworkers
 sleep 10
 hyper service attach-fip --fip=209.177.93.17 healthworkers
